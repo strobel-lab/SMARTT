096ede974e3e7761209e75c6a93a291d10efff07
Chad Torgerson <cd.torgerson@gmail.com>
2018-09-28 14:20:18 -0400

    Changed one more instance of SMART to SMARTT

--- SMARTT - Data Analysis Pipeline/GenerateFits.R
+++ SMARTT - Data Analysis Pipeline/GenerateFits.R
@@ -11,14 +11,14 @@
 ###################################################################################
 # GPL statement:
 #
-# This file is part of SMART-DAP (SMART - Data Analysis Pipeline).
+# This file is part of SMART-DAP (SMARTT - Data Analysis Pipeline).
 # 
-# SMART-DAP is free software: you can redistribute it and/or modify
+# SMARTT-DAP is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
 #
-# SMART-DAP is distributed in the hope that it will be useful,
+# SMARTT-DAP is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
