bb9925e96c400aeccb3972401b7b92b91841c6c6
Kathrynmb <63864424+Kathrynmb@users.noreply.github.com>
2021-02-26 10:03:53 -0500

    Create cutting.sh

---
+++ SMARTT - Data Analysis Pipeline/cutting.sh
@@ -0,0 +1,26 @@
+# usage: cutting.sh <name of paired file1 to be trimmed> 
+#<name of paired file2 to be trimmed> <base name for output files>
+sample_dir=$1
+fastq_file1=$2
+fastq_file2=$3
+file_base=$4
+
+ADAPTER_FWD_5=^NNNNNNNNNNTATATGAGCATCGAAAGAA
+ADAPTER_FWD_3=CTGTAGGCACCATCAATC
+ADAPTER_REV_5=^NNNNNNNNNNGATTGATGGTGCCTACAG
+ADAPTER_REV_3=TTCTTTCGATGCTCATATA
+
+output_dir=/ysm-gpfs/pi/strobel/kmb242/Aax_SMARTT_210120/trimmed/final
+
+input1="$sample_dir"/"$fastq_file1"
+input2="$sample_dir"/"$fastq_file2"
+output1="$output_dir"/"$file_base"_trimmed1.fastq.gz
+output2="$output_dir"/"$file_base"_trimmed2.fastq.gz
+error_out="$output_dir"/"$file_base"_report
+
+#this is for removing the 5' and 3' adaptors from the sample reads
+# -q = quality score cutoff (trims 3' end sequence - 5 nt)
+# -m = minimum length of the sequence AFTER removing adapters
+# -O = min overlap cutoff (adaptor overlap)
+
+cutadapt -m 45 -O 5 -a NNNNNNNNNNTATATGAGCATCGAAAGAA...CTGTAGGCACCATCAATC -A NNNNNNNNNNGATTGATGGTGCCTACAG...TTCTTTCGATGCTCATATA -o "$output1" -p "$output2" --pair-filter=any "$input1" "$input2"
