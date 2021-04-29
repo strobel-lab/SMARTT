f911d578ce1b79a6934d91ebc8a74361a262926b
Kathrynmb <63864424+Kathrynmb@users.noreply.github.com>
2021-04-29 18:03:27 -0400

    Update 2021HeatMapMaker.py

--- SMARTT - Heat Map Maker/2021HeatMapMaker.py
+++ SMARTT - Heat Map Maker/2021HeatMapMaker.py
@@ -1,7 +1,29 @@
 
+# Python script for creating heat maps based on the analysis performed using
+# SMARTT-DAP (SMARTT - Data Analysis Pipeline)
+#
+# - Requires Python 3 (has been tested with version 3.5)
+# - Public release 2.0
+# - Copyright 2021 Kathryn Barth
 
-
-
+###################################################################################
+# GPL statement:
+#
+# This file is part of SMARTT - Heat Map Maker.
+# 
+# SMARTT - Heat Map Maker is free software: you can redistribute it  
+# and/or modify it under the terms of the GNU General Public License 
+# as published by the Free Software Foundation, either version 3 of 
+# the License, or (at your option) any later version.
+#
+# SMARTT - Heat Map Maker is distributed in the hope that it will be 
+# useful, but WITHOUT ANY WARRANTY; without even the implied warranty 
+# of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar.  If not, see <http://www.gnu.org/licenses/>.
+###################################################################################
 
 
 
